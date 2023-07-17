5d4624914cbd2e5631f33160aebdb3d6d3976412
Makala Mabotja <138364127+MakalaMabotja@users.noreply.github.com>
2023-07-17 08:14:15 +0200

    Machine Learning Algorithm based on Audiobook data

---
+++ TF Business Case.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jul 14 06:49:45 2023
+
+@author: 27722
+"""
+
+import pandas as pd
+import numpy as np
+import tensorflow as tf
+import tensorflow_datasets as tfds
+
+raw_data = pd.read_csv('Audiobooks_data.csv')
